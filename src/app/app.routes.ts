416ac90eb2ed6ca72d10ba67096d8697c1aed6c5
jackoschoonderwoerd <34942572+jackoschoonderwoerd@users.noreply.github.com>
2023-11-25 12:45:08 +0100

    add  contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AtelierComponent } from './pages/atelier/atelier.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 
 export const routes: Routes = [
   {
     path:'home', component:HomeComponent
-
+  },
+  {
+    path:'atelier', component:AtelierComponent
+  },
+  {
+    path:'contact', component: ContactComponent
   }
 ];
